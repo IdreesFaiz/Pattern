c900e1b88b2f9b1dff8faf77ae3d10d25f7d8ee5
M IDREES FAIZ <idreesfaiz63@gmail.com>
2024-08-30 22:54:09 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,48 +1,4 @@
-// Pattern ## 1
-for (let i = 0; i < 4; i++) {
-  for (let j = 0; j < 5; j++) {
-    document.write("*");
-  }
-  document.write("<br>");
-} 
-// output
-// *****
-// *****
-// *****
-// *****
 
-  document.write("<br><br><br>");
-
-// Pattern ## 2
-for (let i = 1; i < 6; i++) {
-  for (let j = 0; j < 5; j++) {
-    document.write(i);
-  }
-  document.write("<br>");
-} 
-// output
-// 11111
-// 22222
-// 33333
-// 44444
-// 55555
-
-  document.write("<br><br><br>");
-
-
-// Pattern ## 3
-for (let i = 1; i < 6; i++) {
-  for (let j = 1; j < 6; j++) {
-    document.write(j);
-  }
-  document.write("<br>");
-} 
-//output
-// 12345
-// 12345
-// 12345
-// 12345
-// 12345
 
 
 
