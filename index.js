16d8cc8cf68a8360c9c63f729c96a12506949142
M IDREES FAIZ <idreesfaiz63@gmail.com>
2024-08-30 22:54:37 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,52 @@
+// Pattern ## 1
+for (let i = 0; i < 4; i++) {
+  for (let j = 0; j < 5; j++) {
+    document.write("*");
+  }
+  document.write("<br>");
+}
+// output
+// *****
+// *****
+// *****
+// *****
 
+document.write("<br><br><br>");
 
+// Pattern ## 2
+for (let i = 1; i < 6; i++) {
+  for (let j = 0; j < 5; j++) {
+    document.write(i);
+  }
+  document.write("<br>");
+}
+// output
+// 11111
+// 22222
+// 33333
+// 44444
+// 55555
 
+document.write("<br><br><br>");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Pattern ## 3
+for (let i = 1; i < 6; i++) {
+  for (let j = 1; j < 6; j++) {
+    document.write(j);
+  }
+  document.write("<br>");
+}
+//output
+12345;
+12345;
+12345;
+12345;
+12345;
 
 // for (let i = 1; i < 5+2; i++) {
 //   for (let j = 0; j < i; j++) {
 //     document.write("*");
-    
+
 //   }
 //   document.write("<br>")
 // }
@@ -28,4 +55,3 @@
 // 23
 // 456
 // 78910
-
