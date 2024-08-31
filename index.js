5b1b9ce81f5db7c7b6f66e017e63d21694b99626
Solution Provider <idreesfaiz63@gmail.com>
2024-08-31 22:40:02 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,11 @@
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// for (let i = 1; i < 5+2; i++) {
-//   for (let j = 0; j < i; j++) {
-//     document.write("*");
+for (let i = 1; i < 5+2; i++) {
+  for (let j = 0; j < i; j++) {
+     document.write("*");
     
-//   }
-//   document.write("<br>")
-// }
+   }
+document.write("<br>")
+ }
 //output
 // 1
 // 23
